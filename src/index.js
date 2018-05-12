db490228a65f45ef7c48eeb24a8ace59165e82b7
Alfredo Vargas <vargas.alfredojr@gmail.com>
2018-05-11 23:12:40 -0700

    app to heroku

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import
     BrowserRouter
 } from "react-router-dom";
 import App from "./App";
-import BodyBackgroundColor from "react-body-backgroundcolor";
+// import BodyBackgroundColor from "react-body-backgroundcolor";
 
 /**
  * The main component that drives the app
  */
 ReactDOM.render(
-    <BodyBackgroundColor backgroundColor="#ecebe8">
+    // <BodyBackgroundColor backgroundColor="#ecebe8">
         <BrowserRouter style={{ backgroundColor: "#ecebe8", height: window.innerHeight }}>
             <App />
-        </BrowserRouter>
-   </BodyBackgroundColor>,
+        </BrowserRouter>,
+//    </BodyBackgroundColor>,
 
     document.getElementById("root"),
 );
