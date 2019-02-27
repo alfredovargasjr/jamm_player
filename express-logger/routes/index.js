993452527717a2e8d1a111a4947deadc54c013ac
matthewnguyen <matthewnguyen19@gmail.com>
2019-02-27 15:19:46 -0800

    log routes

--- express-logger/routes/index.js
+++ express-logger/routes/index.js
@@ -4,13 +4,70 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
-router.get('/log', (req, res, next) => {
-  fs.writeFile('logs', `${req.query.message} - ` + (new Date()))
+router.post('/home', async (req, res, next) => {
+    //fs.writeFile('logs', `${req.body.message} - ` + (new Date()));
+    await fs.appendFile('logs', "\n" + "Home - " + (new Date()), function (err) {
+        if (err) throw err;
+    });
 
-  res.status(200).send("ok");
-})
+    await res.status(200).json({status: 'ok'});
+});
+
+router.post('/create', async (req, res, next) => {
+    //fs.writeFile('logs', `${req.body.message} - ` + (new Date()));
+    await fs.appendFile('logs', "\n" + "Create - " + (new Date()), function (err) {
+        if (err) throw err;
+    });
+
+    await res.status(200).json({status: 'ok'});
+});
+
+router.post('/join', async (req, res, next) => {
+    //fs.writeFile('logs', `${req.body.message} - ` + (new Date()));
+    await fs.appendFile('logs', "\n" + "Join - " + (new Date()), function (err) {
+        if (err) throw err;
+    });
+
+    await res.status(200).json({status: 'ok'});
+});
+
+router.post('/search', async (req, res, next) => {
+    //fs.writeFile('logs', `${req.body.message} - ` + (new Date()));
+    await fs.appendFile('logs', "\n" + "Search - " + (new Date()), function (err) {
+        if (err) throw err;
+    });
+
+    await res.status(200).json({status: 'ok'});
+});
+
+router.post('/addTrack', async (req, res, next) => {
+    //fs.writeFile('logs', `${req.body.message} - ` + (new Date()));
+    await fs.appendFile('logs', "\n" + "Add Track - " + (new Date()), function (err) {
+        if (err) throw err;
+    });
+
+    await res.status(200).json({status: 'ok'});
+});
+
+router.post('/suggest', async (req, res, next) => {
+    //fs.writeFile('logs', `${req.body.message} - ` + (new Date()));
+    await fs.appendFile('logs', "\n" + "Suggest Track - " + (new Date()), function (err) {
+        if (err) throw err;
+    });
+
+    await res.status(200).json({status: 'ok'});
+});
+
+router.post('/joinCode', async (req, res, next) => {
+    //fs.writeFile('logs', `${req.body.message} - ` + (new Date()));
+    await fs.appendFile('logs', "\n" + "Join Attempt - " + (new Date()), function (err) {
+        if (err) throw err;
+    });
+
+    await res.status(200).json({status: 'ok'});
+});
 
 module.exports = router;
